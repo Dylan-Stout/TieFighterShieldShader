3d5c7bbe80af4631fdc007a14031df4208771479
Dylan Stout <dylan.d.stout@gmail.com>
2019-11-24 17:39:53 -0700

    Initial project files

---
+++ Assets/Laser/LaserMovementBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserMovementBehaviour : MonoBehaviour
+{
+    public float laserSpeed = 100.0f; 
+
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * laserSpeed; 
+    }
+}
---
+++ Assets/Laser/LaserSpawnBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSpawnBehaviour : MonoBehaviour
+{
+    public float shotCooldown = 3.0f;
+    private float shotCooldownTimer = 3.0f;
+    public GameObject laserPrefab;
+    // Update is called once per frame
+    void Update()
+    {
+        shotCooldownTimer += Time.deltaTime; 
+        // only shoot a laser when cooldown has expired
+        if(shotCooldown < shotCooldownTimer)
+        {
+            // spawn laser and shoot straight forward from the transform that this script is attached to 
+            // (laser movement is handled by the prefab) 
+            Instantiate(laserPrefab, transform.position, transform.rotation);
+            // reset cooldown
+            shotCooldownTimer = 0.0f; 
+        }
+    }
+}
